2285e8c99e5a834197c1186a9445d058550f2702
soloweyDev <solowey@narod.ru>
2020-09-13 21:37:17 +0300

    Initial commit

---
+++ RaceConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace RaceConsoleApp
+{
+    class Program
+    {
+        static int number = 0;
+        static Random random = new Random();
+        static void Main()
+        {
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; ++i)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(Start));
+                threads[i].Start();
+            }
+        }
+
+        static void Start(object _)
+        {
+            int sleep = random.Next(50, 100);
+            number++;
+            Console.WriteLine($"number: {number}, sleep: {sleep}");
+            Runner(number, sleep);
+        }
+
+        static void Runner(int number, int sleep)
+        {
+            for (int i = 0; i < 120; ++i)
+            {
+                Console.Write(number);
+                Thread.Sleep(sleep);
+            }
+
+            Console.WriteLine($"\nnumber: {number} finished");
+        }
+    }
+}
